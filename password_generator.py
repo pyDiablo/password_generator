5075b6bd6334d5bbc76393eef1e527915c00adfe
Salman <49705418+pyDiablo@users.noreply.github.com>
2021-01-18 19:16:20 +0500

    Initial commit

---
+++ password_generator.py
@@ -0,0 +1,40 @@
+"""
+
+    Password Generator
+    Written by: @pyDiablo
+
+    Generates a password of a given length
+    and copies it to the clipboard.
+
+"""
+
+import random
+import pyperclip
+
+
+# Constants
+CHARACTER_SET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890,.<>?;:"=+-_*&^%$#@!`~'  # Length of the string is 84
+
+
+def generate_password(length):
+    """ Generates a password of a given length and copies it to the clipboard """
+
+    password = ''
+    print(len(CHARACTER_SET))
+    for i in range(length):
+        num = random.randint(0, len(CHARACTER_SET) - 1)  # Creates a random integer between 0 and 83 (both inclusive)
+        password += CHARACTER_SET[num]  # Appends a random character from character set to the end of the password
+    return password
+
+
+def main():
+    password_length = int(input('\nPassword length: '))  # Length of password to be generated
+    print('Generating...')
+    password = generate_password(length=password_length)
+    print(password)
+    pyperclip.copy(password)  # Copy password to clipboard
+    print('Password copied to clipboard!')
+
+
+if __name__ == '__main__':
+    main()
