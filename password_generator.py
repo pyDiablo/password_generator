6f5d4d3f95c773e01daa2063351086759662521e
pyDiablo <babars95@yahoo.com>
2021-01-19 13:47:44 +0500

    Exception Handling and Add CLI Support

--- password_generator.py
+++ password_generator.py
@@ -10,6 +10,7 @@
 
 import random
 import pyperclip
+import sys
 
 
 # Constants
@@ -33,17 +34,33 @@ def copy_to_clipboard(some_string):
     print('Password copied to clipboard!')
 
 
-def main():
-    try:
-        password_length = int(input('\nPassword length: '))  # Length of password to be generated
-    except ValueError:
-        print("Error: password's length should be an integer!")  # Print this if user enters any value other than integer
+def main(length=None):
+    # When no argument is provided
+    if length is None:
+        try:
+            password_length = int(input('\nPassword length: '))  # Length of password to be generated
+        except ValueError:
+            print("Error: password's length should be an integer!")  # Print this if user enters any value other than integer
+        else:
+            print('Generating...')
+            password = generate_password(password_length)
+            print(password)
+            copy_to_clipboard(password)
+    # When argument is provided
     else:
-        print('Generating...')
-        password = generate_password(length=password_length)
-        print(password)
-        copy_to_clipboard(password)
+        try:
+            password_length = int(length)
+        except ValueError:
+            print("Error: password's length should be an integer!")  # Print this if user enters any value other than integer
+        else:
+            print('Generating...')
+            password = generate_password(password_length)
+            print(password)
+            copy_to_clipboard(password)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main(sys.argv[1])  # When user provides a command line argument
+    except IndexError:
+        main()  # When no argument is provided
